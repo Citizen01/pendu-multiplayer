1d892ce33f9bef193798f2a57d567078f4c7bd6f
Citizen <jeanbaptiste.e@hotmail.fr>
2012-12-02 17:42:36 +0100

    - Clean project with an empty main and needed includes
    Signed-off-by: Citizen <jeanbaptiste.e@hotmail.fr>

---
+++ pendu-multiplayer/main.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <fmod.h>
+ 
+int main(int argc, char *argv[])
+{
+    
+	return 0;
+}
