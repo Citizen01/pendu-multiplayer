d5b25eda6612b859334ed795584ea9c3efdd7c5b
Citizen <jeanbaptiste.e@hotmail.fr>
2012-12-03 12:17:19 +0100

    - Added few images and modified the background.png - Modified the window size to 960x966
    Signed-off-by: Citizen <jeanbaptiste.e@hotmail.fr>

---
+++ pendu-multiplayer/constantes.h
@@ -0,0 +1,19 @@
+/*
+constantes.h
+------------
+
+Par mateo21, pour Le Site du Zér0 (www.siteduzero.com)
+
+Rôle : définit des constantes communes à tout le programme (taille de la fenêtre...)
+*/
+
+#ifndef DEF_CONSTANTES
+#define DEF_CONSTANTES
+
+	#define LARGEUR_FENETRE     960
+	#define HAUTEUR_FENETRE     658
+
+	enum {HAUT, BAS, GAUCHE, DROITE};
+	enum {VIDE, MUR, CAISSE, OBJECTIF, MARIO, CAISSE_OK};
+
+#endif
